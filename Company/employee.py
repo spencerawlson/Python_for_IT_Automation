236456bfd42027989e153687e6f528fd1615673b
spencerawlson <spencerawlson.spad@gmail.com>
2026-09-14 12:25:05 -0400

    Delete Company directory

--- Company/employee.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-    def __init__(self, fname : str, lname : str, salary : float):
-        self.fname = fname
-        self.lname = lname
-        self.salary = salary
-
-    def calculate_paycheck(self):
-        return self.salary / 52
